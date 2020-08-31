539ba99a99ff718086fd36d8e8c55ec96a9951ac
Skylaunch <maximkagrig@gmail.com>
2020-08-31 18:59:05 +0300

    workable project

---
+++ TODO_List/Action.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TODO_List
+{
+    /// <summary>
+    /// Логика взаимодействия для Action.xaml
+    /// </summary>
+    public partial class Action : UserControl
+    {
+        private StackPanel myStackPanel;
+        private Grid myGrid;
+        private ActionList myActionList;
+
+        //Конструктор с текстом из текстблока в качестве аргумента
+
+        public Action(string ActionText)
+        {
+            InitializeComponent();
+            ActionDescription.Text = ActionText;
+
+            DeleteActionImage.Visibility = Visibility.Hidden;
+        }
+
+        private void DeleteActionImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            //Через пододную конструктору получаем ссылки на панель-родитель элемента и объект класса ActionList
+
+            myStackPanel = (StackPanel)this.Parent;
+            myGrid = (Grid)myStackPanel.Parent;
+            myActionList = (ActionList)myGrid.Parent;
+
+            myStackPanel.Children.Remove(this);
+            myActionList.allActions.Remove(this);
+
+            if (myStackPanel.Children.Count == 0)
+            {
+                myActionList.HideElements();
+            }
+
+            if (IsDone.IsChecked == false)
+            {
+                myActionList.countActions--;
+                myActionList.CountActionsLabel.Content = "Actions left: " +
+                    (myActionList.countActions - myActionList.countCompletedActions);
+            }
+        }
+
+        public void IsDone_Checked(object sender, RoutedEventArgs e)
+        {
+            myStackPanel = (StackPanel)this.Parent;
+            myGrid = (Grid)myStackPanel.Parent;
+            myActionList = (ActionList)myGrid.Parent;
+
+            myActionList.countCompletedActions++;
+            myActionList.CountActionsLabel.Content = "Actions left: " +
+                (myActionList.countActions - myActionList.countCompletedActions);
+
+            //Активация перечёркнутого текста
+
+            ActionDescription.TextDecorations = TextDecorations.Strikethrough;
+
+            /*Тут идет проверка на то, не сортируется ли панель. Без неё при активной SelectedActiveRadioButton 
+            цикл падает из-за того, что коллекция изменяется во время его действия*/
+            if (myActionList.actionsSorting == false)
+            {
+                myActionList.FilterRadioButton_Checked(new RadioButton(), e);
+            }
+        }
+
+        private void IsDone_Unchecked(object sender, RoutedEventArgs e)
+        {
+            //Данная строка нужна для того, чтобы действие мгновенно уходило в нужную категорию
+
+            myActionList.FilterRadioButton_Checked(new RadioButton(), e);
+
+            myActionList.countCompletedActions--;
+            myActionList.CountActionsLabel.Content = "Actions left: " +
+                (myActionList.countActions - myActionList.countCompletedActions);
+
+            //Деактивация перечёркнутого текста
+
+            ActionDescription.TextDecorations = null;
+        }
+
+        // Показ/Скрытие кнопки удаления при наведении на действие
+
+        private void ActionGrid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DeleteActionImage.Visibility = Visibility.Visible;
+        }
+
+        private void ActionGrid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            DeleteActionImage.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ TODO_List/ActionList.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TODO_List
+{
+    /// <summary>
+    /// Логика взаимодействия для ActionList.xaml
+    /// </summary>
+    public partial class ActionList : UserControl
+    {
+        public ActionList()
+        {
+            InitializeComponent();
+        }
+
+        //Поле для проверки сортировки ВСЕЙ панели
+        public bool actionsSorting = false;
+
+        //Лист со всеми действиями для организации фильтрации
+        public List<Action> allActions = new List<Action>();
+
+        private void ShowElements()
+        {
+            SelectAllImage.Visibility = Visibility.Visible;
+            ListStackPanel.Visibility = Visibility.Visible;
+            CountActionsLabel.Visibility = Visibility.Visible;
+            SelectActiveRadioButton.Visibility = Visibility.Visible;
+            SelectAllRadioButton.Visibility = Visibility.Visible;
+            SelectCompletedRadioButton.Visibility = Visibility.Visible;
+        }
+
+        public void HideElements()
+        {
+            SelectAllImage.Visibility = Visibility.Hidden;
+            ListStackPanel.Visibility = Visibility.Hidden;
+            CountActionsLabel.Visibility = Visibility.Hidden;
+            SelectActiveRadioButton.Visibility = Visibility.Hidden;
+            SelectAllRadioButton.Visibility = Visibility.Hidden;
+            SelectCompletedRadioButton.Visibility = Visibility.Hidden;
+        }
+
+        public int countActions = 0;
+        public int countCompletedActions = 0;
+
+        private void ActionTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && ActionTextBox.Text.Length != 0)
+            {
+                Action newAction = new Action(ActionTextBox.Text);
+                ListStackPanel.Children.Add(newAction);
+                ActionTextBox.Text = "";
+
+                allActions.Add(newAction);
+
+                ShowElements();
+
+                countActions++;
+                CountActionsLabel.Content = "Actions left:" + (countActions - countCompletedActions);
+            }
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            HideElements();
+        }
+
+        private void SelectAllImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //Вот место, где происходит сортировка ВСЕЙ панели
+
+            actionsSorting = true;
+
+            foreach (Action action in ListStackPanel.Children)
+            {
+                action.IsDone.IsChecked = true;
+            }
+
+            actionsSorting = false;
+
+            //Так как во время цилка в классе Action из-за actionSorting == true фильтрации не происходит, то 
+            //её нужно запустить после цикла вручную
+            FilterRadioButton_Checked(new RadioButton(), e);
+        }
+
+        public void FilterRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            //Проверка на наличия хотя бы одного действия
+            if(allActions.Count == 0)
+            {
+                return;
+            }
+
+            ListStackPanel.Children.Clear();
+
+            //Фильтрация панели исходя из активной RadioButton
+
+            if (SelectAllRadioButton.IsChecked == true)
+            {
+                foreach (Action action in allActions)
+                {
+                    ListStackPanel.Children.Add(action);
+                }
+            }
+            else if (SelectActiveRadioButton.IsChecked == true)
+            {
+                foreach (Action action in allActions)
+                {
+                    if (action.IsDone.IsChecked == false)
+                    {
+                        ListStackPanel.Children.Add(action);
+                    }
+                }
+            }
+            else
+            {
+                foreach (Action action in allActions)
+                {
+                    if (action.IsDone.IsChecked == true)
+                    {
+                        ListStackPanel.Children.Add(action);
+                    }
+                }
+            }
+        }
+    }
+}
